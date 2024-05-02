492039333afb5a37f6f7caf3f7791b54b89c9bdd
AntonCSalvador <antoncsalvador@gmail.com>
2024-05-01 21:24:14 -0400

    fixed alt text glitch

--- frontend/src/pages/ProfilePage.js
+++ frontend/src/pages/ProfilePage.js
@@ -100,7 +100,7 @@ function ProfilePage() {
 
     const booksFormatted = data.map(book => ({
       imageUrl: book.book_cover_image,
-      altText: book.title || "No title available", // Assuming 'title' is another field in your data
+      altText: book.book_name || "No title available", // Assuming 'title' is another field in your data
       // userBookId: book.userBookId,
       id: book.id,
     }));
